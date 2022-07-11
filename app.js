3e711902f06235d4cc7775cf8da2298d3c0dd0c0
Jose María Arias Gallego-Acho <sr.conejo@icloud.com>
2022-07-11 22:17:12 +0200

    Added some code

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 function Game() {
-  this.started = false;
-  this.score = 0;
+  (this.started = false),
+    (this.score = 0),
+    (this.start = function () {
+      if (this.started == true) return;
+      document.getElementById("crd-holder").innerHTML = ``;
+    });
 }
