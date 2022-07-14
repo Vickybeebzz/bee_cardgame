18aaa97a1a38ad573bf152191e1127a35c1b0de0
Jose María Arias Gallego-Acho <sr.conejo@icloud.com>
2022-07-14 14:25:26 +0200

    Refactored multiple functions into separate

--- app.js
+++ app.js
@@ -1,102 +1,79 @@
 function Game() {
   this.started = false;
-  this.previouscard = null;
-  this.previouscardid = 0;
+  this.timeout = 0;
+  this.previousCard = null;
+  this.previousCardId = 0;
   this.score = 0;
   this.flips = 0;
-  this.clickenabled = false;
+  this.clickEnabled = false;
   this.deck = new Deck();
 
   this.start = function () {
     if (this.started == true) return;
-    this.deck.shuffle();
-    this.clickenabled = true;
+    shuffle(this.deck.cards);
+    this.clickEnabled = true;
+
     for (let k = 0; k < 16; k++) {
       document
-        .getElementById("card" + (k + 1))
+        .getElementById("card" + k)
         .addEventListener("click", () => game.flip(k));
+    }
+    for (k = 0; k < 16; k++) {
+      document.getElementById("crd" + k).src = this.deck.cards[k].image;
     }
 
     this.started = true;
   };
 
   this.reset = function () {
+    clearTimeout(this.timeout);
     this.started = false;
     this.score = 0;
     this.flips = 0;
-    if (localStorage.getItem("hiScore") == null)
-      document.getElementById("scorebox").innerHTML =
-        `Moves Used: ${this.flips}` + "<br />" + `Best: 0`;
-    else {
-      document.getElementById("scorebox").innerHTML =
-        `Moves Used: ${this.flips}` +
-        "<br />" +
-        `Best: ${parseInt(localStorage.getItem("hiScore"))}`;
-    }
+    updateScore();
     this.deck = new Deck();
     document.getElementById("crd-holder").innerHTML = crdholder;
-    this.clickenabled = true;
+    this.clickEnabled = true;
     this.start();
   };
 
   this.flip = function (id) {
-    if (this.clickenabled == false) return;
-    if (this.deck.cards[id].flipped == false && this.previouscard == null) {
-      document.getElementById("crd" + (id + 1)).src = this.deck.cards[id].image;
-      document.getElementById("cardi" + (id + 1)).classList.toggle("flipped");
+    if (this.clickEnabled == false) return;
+    if (this.deck.cards[id].flipped == false && this.previousCard == null) {
+      this.toggleFlip(id);
       this.deck.cards[id].flipped = true;
-      this.previouscard = this.deck.cards[id];
-      this.previouscardid = id + 1;
+      this.previousCard = this.deck.cards[id];
+      this.previousCardId = id;
     } else if (this.deck.cards[id].flipped == false) {
-      document.getElementById("crd" + (id + 1)).src = this.deck.cards[id].image;
-      document.getElementById("cardi" + (id + 1)).classList.toggle("flipped");
+      this.toggleFlip(id);
       this.deck.cards[id].flipped = true;
-      if (this.deck.cards[id].value == this.previouscard.value) {
+      if (this.deck.cards[id].value == this.previousCard.value) {
         this.score = this.score + 1;
-        this.previouscard = null;
+        this.previousCard = null;
         if (this.score == 8) {
           if (
             this.flips < parseInt(localStorage.getItem("hiScore")) ||
             localStorage.getItem("hiScore") == null
           )
-            setTimeout(() => {
-              localStorage.setItem("hiScore", game.flips);
-              document.getElementById("scorebox").innerHTML =
-                `Moves Used: ${this.flips}` +
-                "<br />" +
-                `Best: ${parseInt(localStorage.getItem("hiScore"))}`;
-            }, 1000);
+            setTimeout(updateScore, 1000);
         }
       } else {
-        this.clickenabled = false;
-        setTimeout(() => {
+        this.clickEnabled = false;
+        this.timeout = setTimeout(() => {
           this.deck.cards[id].flipped = false;
-          this.previouscard.flipped = false;
-          document.getElementById("crd" + (id + 1)).src =
-            "./images/cardback.png";
-          document
-            .getElementById("cardi" + (id + 1))
-            .classList.toggle("flipped");
-          document.getElementById("crd" + this.previouscardid).src =
-            "./images/cardback.png";
-          document
-            .getElementById("cardi" + this.previouscardid)
-            .classList.toggle("flipped");
-          this.previouscard = null;
-          this.clickenabled = true;
+          this.previousCard.flipped = false;
+          this.toggleFlip(id);
+          this.toggleFlip(this.previousCardId);
+          this.previousCard = null;
+          this.clickEnabled = true;
         }, 1000);
       }
     } else return;
     this.flips = this.flips + 1;
-    if (localStorage.getItem("hiScore") == null)
-      document.getElementById("scorebox").innerHTML =
-        `Moves Used: ${this.flips}` + "<br />" + `Best: 0`;
-    else {
-      document.getElementById("scorebox").innerHTML =
-        `Moves Used: ${this.flips}` +
-        "<br />" +
-        `Best: ${parseInt(localStorage.getItem("hiScore"))}`;
-    }
+    updateScore();
+  };
+  this.toggleFlip = function (id) {
+    document.getElementById("cardi" + id).classList.toggle("flipped");
   };
 }
 
@@ -107,48 +84,40 @@ function Card(value) {
 }
 
 function Deck() {
-  this.cards = [
-    new Card(1),
-    new Card(1),
-    new Card(2),
-    new Card(2),
-    new Card(3),
-    new Card(3),
-    new Card(4),
-    new Card(4),
-    new Card(5),
-    new Card(5),
-    new Card(6),
-    new Card(6),
-    new Card(7),
-    new Card(7),
-    new Card(8),
-    new Card(8),
-  ];
-  this.shuffle = function () {
-    let j = 0;
-    let temp;
+  this.cards = [];
 
-    for (let i = this.cards.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      temp = this.cards[i];
-      this.cards[i] = this.cards[j];
-      this.cards[j] = temp;
-    }
-  };
+  for (i = 0; i < 16; i++) {
+    this.cards[i] = new Card(Math.round((i + 1) / 2));
+  }
+}
+
+function shuffle(cards) {
+  let j = 0;
+  let temp;
+
+  for (let i = cards.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    temp = cards[i];
+    cards[i] = cards[j];
+    cards[j] = temp;
+  }
+}
+
+function updateScore() {
+  if (localStorage.getItem("hiScore") == null) {
+    document.getElementById("scorebox").innerHTML =
+      `Moves Used: ${game.flips}` + "<br />" + `Best: 0`;
+    const crdholder = document.getElementById("crd-holder").innerHTML;
+  } else {
+    document.getElementById("scorebox").innerHTML =
+      `Moves Used: ${game.flips}` +
+      "<br />" +
+      `Best: ${parseInt(localStorage.getItem("hiScore"))}`;
+  }
 }
 
 let game = new Game();
-if (localStorage.getItem("hiScore") == null) {
-  document.getElementById("scorebox").innerHTML =
-    `Moves Used: ${game.flips}` + "<br />" + `Best: 0`;
-  const crdholder = document.getElementById("crd-holder").innerHTML;
-} else {
-  document.getElementById("scorebox").innerHTML =
-    `Moves Used: ${game.flips}` +
-    "<br />" +
-    `Best: ${parseInt(localStorage.getItem("hiScore"))}`;
-}
+updateScore();
 const crdholder = document.getElementById("crd-holder").innerHTML;
 
 console.log(game.deck);
